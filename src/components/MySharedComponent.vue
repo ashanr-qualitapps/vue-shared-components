768611ef1931a3b2238901e59bb81d693596f9ec
ashanr-qualitapps <ashan.rajapaksha@qualitapps.com>
2024-07-17 03:14:49 +0000

    vue shared compoent added

---
+++ src/components/MySharedComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-shared-component">
+    <h1>This is a shared component!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MySharedComponent',
+}
+</script>
+
+<style scoped>
+.my-shared-component {
+  color: blue;
+}
+</style>
