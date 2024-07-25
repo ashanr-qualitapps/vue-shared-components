fbe08afad3c9abbe3d5bc0ba3b474282b5801897
Ashan R <ashan.rajapaksha@qualitapps.com>
2024-07-25 13:13:39 +0530

    LoginForm added to index export

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // Import your components
 
 import CountryList from './components/CountryList.vue';
-
+import LoginForm from './components/LoginForm.vue';
 // Export them
 export {  CountryList };
 
 // Optionally, provide a default export for easier import
 export default {
-  CountryList
+  CountryList,
+  LoginForm
 };
